96cf1e1e0acedb52e143e10420ee6cabe73b7eb6
Chanho Kim <kiss.yagni.dry@gmail.com>
2022-12-25 00:13:52 +0900

    initial Setting

---
+++ src/01/RCC.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class RCC extends Component {
+  render() {
+    let text = 'DoubleQuate';
+    return <div name="name">{text}</div>;
+  }
+}
